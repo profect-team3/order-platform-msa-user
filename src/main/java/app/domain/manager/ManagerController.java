d2ee7f33ceac1d6f019fce878213ccdeb992940e
huniieee <sehoon981014@gmail.com>
2025-08-16 16:00:02 +0900

    chore(user) domain/user requestparam->pathvariable
    선택한 유저 정보 조회 controller 수정

--- src/main/java/app/domain/manager/ManagerController.java
+++ src/main/java/app/domain/manager/ManagerController.java
@@ -35,22 +35,21 @@ public class ManagerController {
 		summary = "전체 사용자 목록 조회",
 		description = "가입한 사용자 목록을 페이지 별로 조회합니다. 생성일 또는 수정일 기준으로 정렬할 수 있습니다.")
 	public ApiResponse<PagedResponse<GetCustomerListResponse>> getAllCustomer(
-		@PageableDefault(size = 20, sort = "createdAt", direction = DESC) Pageable pageable,
-		@AuthenticationPrincipal UserDetails principal
+		@PageableDefault(size = 20, sort = "createdAt", direction = DESC) Pageable pageable
 	) {
 		return ApiResponse.onSuccess(ManagerSuccessStatus.MANAGER_GET_CUSTOMER_OK,managerService.getAllCustomer(pageable));
 	}
-
 	@GetMapping("/customer/{userId}")
 	@Operation(
 		summary = "선택한 유저 정보 조회",
 		description = "선택한 유저의 자세한 정보와 등록한 주소를 확인 합니다."
 	)
 	public ApiResponse<GetCustomerDetailResponse> getUsersDetailById(
-		@RequestParam("userId") Long userId
+		@PathVariable("userId") Long userId
 	) {
 		return ApiResponse.onSuccess(ManagerSuccessStatus.MANAGER_GET_CUSTOMER_DETAIL_OK,managerService.getCustomerDetailById(userId));
 	}
+
 
 	@GetMapping("/customer/search")
 	@Operation(
