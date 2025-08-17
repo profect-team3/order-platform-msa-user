9cf9e8019cdeda2dec6f48e398587d7692307be3
noeyah2 <hy0708@cau.ac.kr>
2025-08-17 20:21:28 +0900

    feat(auth): UserApplication excludeFilter추가
    - commonsecurity의 SecurityConfig을 빈으로 등록안하도록 해 충돌 막기

--- src/main/java/app/UserApplication.java
+++ src/main/java/app/UserApplication.java
@@ -2,8 +2,19 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@ComponentScan(
+	basePackages = "app",
+	excludeFilters = @ComponentScan.Filter(
+		type = FilterType.ASSIGNABLE_TYPE,
+		classes = app.commonSecurity.config.SecurityConfig.class
+	)
+)
 public class UserApplication {
 
 	public static void main(String[] args) {
