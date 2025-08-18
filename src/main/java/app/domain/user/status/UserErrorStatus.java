b08aa6bb12e13af6a8d0b10a7a967ffe877a4bc1
noeyah2 <hy0708@cau.ac.kr>
2025-08-18 18:33:43 +0900

    feat(address): UserErrorStatus 주소 상태 코드 추가

--- src/main/java/app/domain/user/status/UserErrorStatus.java
+++ src/main/java/app/domain/user/status/UserErrorStatus.java
@@ -10,9 +10,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum UserErrorStatus implements BaseCode {
-
-	INVALID_PASSWORD(HttpStatus.FORBIDDEN, "AUTH_001", "비밀번호가 일치하지 않습니다."),
-
 	USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "USER001", "이미 존재하는 유저입니다."),
 	EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "USER002", "이미 사용 중인 이메일입니다."),
 	NICKNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "USER003", "이미 사용 중인 닉네임입니다."),
@@ -20,7 +17,12 @@ public enum UserErrorStatus implements BaseCode {
 
 	AUTHENTICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "USER005", "인증 정보를 찾을 수 없습니다."),
 	LOGOUT_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "USER006","로그아웃에 실패하여 회원탈퇴를 실패했습니다."),
-	CREATE_USER_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "USER007", "사용자 생성에 실패했습니다.");
+	CREATE_USER_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "USER007", "사용자 생성에 실패했습니다."),
+	ADDRESS_NOT_FOUND(HttpStatus.NOT_FOUND, "ADDRESS001", "해당하는 주소를 찾을 수 없습니다."),
+	ADDRESS_ACCESS_DENIED(HttpStatus.FORBIDDEN, "ADDRESS002", "사용자 권한이 없는 주소입니다."),
+	CANNOT_DELETE_DEFAULT_ADDRESS(HttpStatus.CONFLICT, "ADDRESS003", "기본 주소는 삭제할 수 없습니다. 기본 주소를 변경해주세요."),
+	ADDRESS_ALREADY_DELETED(HttpStatus.GONE, "ADDRESS_410_1", "이미 삭제된 주소는 수정할 수 없습니다.");
+
 	private final HttpStatus httpStatus;
 	private final String code;
 	private final String message;
